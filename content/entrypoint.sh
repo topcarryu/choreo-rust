3bb27e68233990174df92193ac635e91f18de7ee
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-06 20:54:07 +0800

    asdasda

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -12,9 +12,6 @@ cp /usr/src/app/nginx_ss.conf /tmp/nginx_ss.conf
 sed -i "s|WSPATH|${WSPATH}|g;s|PORT|${PORT}|" /tmp/nginx_ss.conf
 cat /tmp/nginx_ss.conf
 
-cd /tmp
-mkdir -p /tmp/nginx-tmp
-
 /usr/src/app/ssserver -c /tmp/ss.json &
 
 exec nginx -c /tmp/nginx_ss.conf 2>&1 > /dev/null
