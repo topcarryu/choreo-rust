fd9e14b74018e1f3248d8aa8319ffa888a5d434d
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-06 19:26:28 +0800

    aaa

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -5,7 +5,7 @@ WSPATH=${WSPATH:-'user'}
 PORT=${PORT:-'3000'}
 
 sed -i "s#WSPATH#${WSPATH}#g;s#PASSWD#${PASSWD}#g" /usr/src/app/ss-config.json > /tmp/ss.json
-sed -i "s#WSPATH#${WSPATH}#g;s#PORT#${PORT}#g" /usr/src/app/nginx_ss.conf > /tmp/ss.conf
+sed -i "s#WSPATH#${WSPATH}#g;s#PORT#${PORT}#g" /usr/src/app/nginx_ss.conf > /tmp/nginx_ss.conf
 
 rm -rf /usr/share/nginx/*
 wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
@@ -13,5 +13,4 @@ unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
 ss-server -c /tmp/ss.json &
-rm -rf /etc/nginx/sites-enabled/default
-nginx -g 'daemon off;'
+nginx -c /tmp/nginx_ss.conf &
