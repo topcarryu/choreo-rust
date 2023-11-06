f7b229d741ba3363600ecf0a9b060c0b0aa79e7f
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-06 19:32:47 +0800

    dfad

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -7,10 +7,5 @@ PORT=${PORT:-'3000'}
 sed -i "s#WSPATH#${WSPATH}#g;s#PASSWD#${PASSWD}#g" /usr/src/app/ss-conf.json > /tmp/ss.json
 sed -i "s#WSPATH#${WSPATH}#g;s#PORT#${PORT}#g" /usr/src/app/nginx_ss.conf > /tmp/nginx_ss.conf
 
-rm -rf /usr/share/nginx/*
-wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
-unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
-rm -f /usr/share/nginx/mikutap.zip
-
 /usr/src/app/ss-server -c /tmp/ss.json &
 nginx -c /tmp/nginx_ss.conf &
