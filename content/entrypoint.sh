c44134a0081fa4c1edc729d41995d47dea7527c4
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-06 19:48:55 +0800

    asdas

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -4,8 +4,11 @@ PASSWD=${PASSWD:-'abcdefg'}
 WSPATH=${WSPATH:-'user'}
 PORT=${PORT:-'3000'}
 
-sed -i "s|WSPATH|${WSPATH}|g;s|PASSWD|${PASSWD}|" /usr/src/app/ss-conf.json && cp /usr/src/app/ss-conf.json /tmp/ss.json
-sed -i "s|WSPATH|${WSPATH}|g;s|PORT|${PORT}|" /usr/src/app/nginx_ss.conf && cp /usr/src/app/nginx_ss.conf /tmp/nginx_ss.conf
+cp /usr/src/app/ss-conf.json /tmp/ss.json
+sed -i "s|WSPATH|${WSPATH}|g;s|PASSWD|${PASSWD}|" /tmp/ss.json
+
+cp /usr/src/app/nginx_ss.conf /tmp/nginx_ss.conf
+sed -i "s|WSPATH|${WSPATH}|g;s|PORT|${PORT}|" /tmp/nginx_ss.conf
 
 /usr/src/app/ssserver -c /tmp/ss.json &
 nginx -c /tmp/nginx_ss.conf &
