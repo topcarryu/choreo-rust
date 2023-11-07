b1520925b46c10c464325cec52e1cdbc21142ebb
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 22:23:34 +0800

    a

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -3,11 +3,10 @@
 set -e
 exec 2>&1
 
-yarn start &
-
 echo $ArgoJSON > /tmp/argo.json
 ARGOID="$(jq .TunnelID /tmp/argo.json | sed 's/\"//g')"
 cp /home/choreouser/argo.yaml /tmp/argo.yaml
 sed -i "s|ARGOID|${ARGOID}|g;s|ARGO_DOMAIN|${ARGO_DOMAIN}|" /tmp/argo.yaml
 
+yarn start &
 argo tunnel --config /tmp/argo.yaml run 2>&1 >/dev/null 
