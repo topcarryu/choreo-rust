f2a3741392a0aaf0b3f5fbfbac925edb3d36dfd3
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 13:10:07 +0800

    a

--- content/install.sh
+++ content/install.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR_TMP="$(mktemp -d)"
 
-wget -O - https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.17.0/shadowsocks-v1.17.0.x86_64-unknown-linux-gnu.tar.xz | tar -zxf - -C ${DIR_TMP}
+wget -O - https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.17.0/shadowsocks-v1.17.0.x86_64-unknown-linux-gnu.tar.xz | tar -xJf - -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/ss* /usr/bin/
 
 wget -O - https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-amd64-v1.3.2.tar.gz | tar -zxf - -C ${DIR_TMP}
