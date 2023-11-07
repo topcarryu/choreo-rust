84f6e7909d9e257c902f37a0abb97842e172395b
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 12:14:06 +0800

    a

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -15,6 +15,6 @@ ARGOID="$(jq .TunnelID /tmp/argo.json | sed 's/\"//g')"
 cp /usr/src/app/argo.yaml /tmp/argo.yaml
 sed -i "s|ARGOID|${ARGOID}|g;s|ARGO_DOMAIN|${ARGO_DOMAIN}|" /tmp/argo.yaml
 
-nohup /usr/src/app/ssserver -c /tmp/ss.json &
-nohup /usr/src/app/cloudflared --loglevel fatal tunnel --edge-ip-version auto --config /tmp/argo.yaml run &
+/usr/src/app/ssserver -c /tmp/ss.json &
+/usr/src/app/cloudflared --loglevel fatal tunnel --edge-ip-version auto --config /tmp/argo.yaml run &
 nohup caddy run --config /usr/src/app/Caddyfile --adapter caddyfile &
