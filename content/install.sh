8d023f2781e7063779603f7ed638e207a078e592
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 13:29:45 +0800

    asdasd

--- content/install.sh
+++ content/install.sh
@@ -4,13 +4,11 @@ set -e
 
 DIR_TMP="$(mktemp -d)"
 
-wget -O - https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.17.0/shadowsocks-v1.17.0.x86_64-unknown-linux-gnu.tar.xz | tar -xJf - -C ${DIR_TMP}
-for file in ${DIR_TMP}/ss*; do
-  install -m 755 "$file" /usr/bin/
-done
+wget -O - https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.17.0/shadowsocks-v1.17.0.x86_64-unknown-linux-musl.tar.xz | tar -xJf - -C ${DIR_TMP}
+install -m 755 ${DIR_TMP}/ssserver /usr/bin/
 
 wget -O - https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-amd64-v1.3.2.tar.gz | tar -zxf - -C ${DIR_TMP}
-install -m 755 ${DIR_TMP}/v2ray* /usr/bin/v2-plugin
+install -m 755 ${DIR_TMP}/v2ray-plugin_linux_amd64 /usr/bin/
 
 busybox wget -qO /usr/bin/argo https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x /usr/bin/argo 
