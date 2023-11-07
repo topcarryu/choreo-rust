588168575508cbf09c5ed4f138515293d5c2793e
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 12:26:04 +0800

    w

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -20,5 +20,5 @@ sed -i "s|ARGOID|${ARGOID}|g;s|ARGO_DOMAIN|${ARGO_DOMAIN}|" /tmp/argo.yaml
 cat /tmp/argo.yaml
 
 /usr/src/app/ssserver -c /tmp/ss.json &
-/usr/src/app/cloudflared --loglevel fatal tunnel --edge-ip-version auto --config /tmp/argo.yaml run 2>&1 >/dev/null
+/usr/src/app/cloudflared tunnel --edge-ip-version auto --config /tmp/argo.yaml run 2>&1 >/dev/null
 caddy run --config /usr/src/app/Caddyfile --adapter caddyfile 2>&1 >/dev/null
