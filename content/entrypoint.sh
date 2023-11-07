168f5531ed8bcb4c153db18da34c4f544c1c20da
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-07 14:36:19 +0800

    asdasd

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -9,15 +9,15 @@ PORT=${PORT:-'3000'}
 ArgoJSON=${ArgoJSON:-'{asdq1}'}
 ARGO_DOMAIN=${ARGO_DOMAIN:-'a.b.c'}
 
-cp /usr/src/app/ss-conf.json /tmp/ss.json
+cp /home/choreouser/ss-conf.json /tmp/ss.json
 sed -i "s|WSPATH|${WSPATH}|g;s|PASSWD|${PASSWD}|" /tmp/ss.json
 cat /tmp/ss.json
 
 echo $ArgoJSON > /tmp/argo.json
 ARGOID="$(jq .TunnelID /tmp/argo.json | sed 's/\"//g')"
-cp /usr/src/app/argo.yaml /tmp/argo.yaml
+cp /home/choreouser/argo.yaml /tmp/argo.yaml
 sed -i "s|ARGOID|${ARGOID}|g;s|ARGO_DOMAIN|${ARGO_DOMAIN}|" /tmp/argo.yaml
 
 ssserver -c /tmp/ss.json &
 argo tunnel --config /tmp/argo.yaml run 2>&1 >/dev/null &
-caddy run --config /usr/src/app/Caddyfile --adapter caddyfile 2>&1 >/dev/null
+caddy run --config /home/choreouser/Caddyfile --adapter caddyfile 2>&1 >/dev/null
