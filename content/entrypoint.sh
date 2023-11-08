b1aa210eb5ff2ab721fbee2253c09cca1e2b42c3
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-08 15:25:32 +0800

    aa

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -3,10 +3,9 @@
 set -e
 exec 2>&1
 
-echo $ArgoJSON > /tmp/argo.json
-ARGOID="$(jq .TunnelID /tmp/argo.json | sed 's/\"//g')"
-cp /home/choreouser/argo.yaml /tmp/argo.yaml
-sed -i "s|ARGOID|${ARGOID}|g;s|ARGO_DOMAIN|${ARGO_DOMAIN}|" /tmp/argo.yaml
-
-argo tunnel --config /tmp/argo.yaml run 2>&1 >/dev/null &
+gost -L socks5://user:pass@:51056?udp=true &
+tailscaled --tun=userspace-networking --socks5-server=localhost:51055 &
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=choreo-us &
+echo Tailscale started
+ALL_PROXY=socks5://localhost:51055/ /app/my-app &
 yarn start 
