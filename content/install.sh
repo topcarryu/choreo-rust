d6d2d817d28aa33a2d78d86f6870f58fb069dd8b
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-08 15:35:03 +0800

    a

--- content/install.sh
+++ content/install.sh
@@ -4,14 +4,14 @@ set -e
 
 DIR_TMP="$(mktemp -d)"
 
-busybox wget -qO /usr/bin/argo https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-chmod +x /usr/bin/argo 
+wget -O - https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20231107/gost_3.0.0-nightly.20231107_linux_amd64v3.tar.gz | tar -zxf - -C ${DIR_TMP}
+install -m 755 ${DIR_TMP}/alist /usr/bin/
 
 wget -O - https://pkgs.tailscale.com/stable/tailscale_1.52.1_amd64.tgz | tar -zxf - -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/tailscale  /usr/bin/
 install -m 755 ${DIR_TMP}/tailscaled /usr/bin/
 
-wget -O - https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20231107/gost_3.0.0-nightly.20231107_linux_amd64v3.tar.gz | tar -zxf - -C ${DIR_TMP}
-install -m 755 ${DIR_TMP}/gost /usr/bin/
+busybox wget -qO /usr/bin/argo https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +x /usr/bin/argo 
 
 rm -rf ${DIR_TMP}
