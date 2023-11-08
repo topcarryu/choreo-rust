c311977a733b95c90270d702ecee6fde426b54a2
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-08 15:36:34 +0800

    aaa

--- content/install.sh
+++ content/install.sh
@@ -5,7 +5,7 @@ set -e
 DIR_TMP="$(mktemp -d)"
 
 wget -O - https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20231107/gost_3.0.0-nightly.20231107_linux_amd64v3.tar.gz | tar -zxf - -C ${DIR_TMP}
-install -m 755 ${DIR_TMP}/alist /usr/bin/
+install -m 755 ${DIR_TMP}/gost /usr/bin/
 
 wget -O - https://pkgs.tailscale.com/stable/tailscale_1.52.1_amd64.tgz | tar -zxf - -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/tailscale  /usr/bin/
