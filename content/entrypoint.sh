afe8cfa07a906fac77a16df07ad573fa4dce8d26
topcarryu <107200176+topcarryu@users.noreply.github.com>
2023-11-08 15:46:14 +0800

    gost

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -7,5 +7,5 @@ gost -L socks5://user:pass@:51056?udp=true &
 tailscaled --tun=userspace-networking --socks5-server=localhost:51055 &
 tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=choreo-us &
 echo Tailscale started
-ALL_PROXY=socks5://localhost:51055/ /app/my-app &
+ALL_PROXY=socks5://localhost:51055/ gost &
 yarn start 
