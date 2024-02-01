07994eb75827edfe2dd93871082df2226eb1887a
topcarryu <ostiepo@hotmail.com>
2024-02-02 01:21:20 +0800

    *

--- files/install.sh
+++ files/install.sh
@@ -5,12 +5,12 @@ DIR_TMP="$(mktemp -d)"
 
 
 wget https://pkgs.tailscale.com/stable/tailscale_1.58.2_amd64.tgz 
-tar zxvf -C ${DIR_TMP}
+tar zxvf tailscale* -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/tailscale*/tailscale /usr/bin/
 install -m 755 ${DIR_TMP}/tailscale*/tailscaled /usr/bin/
 
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.17.1/shadowsocks-v1.17.1.x86_64-unknown-linux-musl.tar.xz 
-tar xf -C ${DIR_TMP}
+tar xf shadowsocks* -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/shadowsocks*/ss* /usr/bin/
 
 rm -rf ${DIR_TMP}
