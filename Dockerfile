385b128d9ca705bceb35e73339b4bcda18aee560
topcarryu <ostiepo@hotmail.com>
2024-02-02 09:20:24 +0800

    12312

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:latest
+FROM alpine
 
 WORKDIR /home/choreouser
 
@@ -6,16 +6,11 @@ COPY files/* /home/choreouser/
 
 ENV PM2_HOME=/tmp
 
-RUN apt-get update &&\
-    apt-get install -y iproute2 vim netcat-openbsd &&\
-    npm install -r package.json &&\
-    npm install -g pm2 &&\
+RUN apk add --no-cache iproute2 vim netcat-openbsd &&\
+    chmod +x install.sh &&\
+    sh install.sh &&\
     addgroup --gid 10001 choreo &&\
-    adduser --disabled-password  --no-create-home --uid 10001 --ingroup choreo choreouser &&\
-    usermod -aG sudo choreouser &&\
-    chmod +x entrypoint.sh install.sh &&\
-    bash install.sh &&\
-    npm install -r package.json
+    adduser --disabled-password  --no-create-home --uid 10001 --ingroup choreo choreouser
 
 ENTRYPOINT [ "node", "server.js" ]
 
--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,38 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e
 exec 2>&1
 
-# 设置各变量
-WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 TAILSCALE_AUTHKEY=${TAILSCALE_AUTHKEY:-'ABCDEFG'}
-
-generate_pm2_file() {
-  cat > /tmp/ecosystem.config.js << EOF
-module.exports = {
-  "apps":[
-      {
-          name: "tailscaled",
-          script: "tailscaled --port=3006 --tun=userspace-networking --socket=/tmp/tailscale/tailscaled.sock --state=/tmp/tailscale/config/tailscaled.state --statedir=/tmp/tailscale/files"
-      },
-      {
-          name: "tailscale",
-          script: "tailscale --socket=/tmp/tailscale/tailscaled.sock up --authkey=${TAILSCALE_AUTHKEY} --ssh=true --accept-dns=true --host-routes=true --netfilter-mode=on --snat-subnet-routes=true --accept-routes=true --advertise-exit-node=true --hostname=choreo"
-      },
-      {
-          name: "ssserver",
-          script: 'ssserver -s "[::]:8088" -m "aes-256-gcm" -k "hello-kitty"'
-      }
-  ]
-}
-EOF
-}
-generate_pm2_file
 
 cd /tmp
 mkdir -p /tmp/tailscale /tmp/tailscale/config /tmp/tailscale/files 
 touch /tmp/tailscale/config/tailscaled.state 
-pm2 start /tmp/ecosystem.config.js &
+
+tailscaled --port=3006 --tun=userspace-networking --socket=/tmp/tailscale/tailscaled.sock --state=/tmp/tailscale/config/tailscaled.state --statedir=/tmp/tailscale/files &
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --ssh=true --accept-dns=true --host-routes=true --netfilter-mode=on --snat-subnet-routes=true --accept-routes=true --advertise-exit-node=true --hostname=choreo &
 tailscale cert choreo.tailnet-e2eb.ts.net &
 tailscale serve --bg 8088
