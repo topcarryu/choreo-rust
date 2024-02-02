028a2441c59847ca8dc6df5e8856dbe7b8930012
topcarryu <ostiepo@hotmail.com>
2024-02-02 10:53:00 +0800

    1231

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -9,11 +9,10 @@ DOMAIN=${DOMAIN:-'a.b'}
 cd /tmp
 
 mkdir -p /tmp/tailscale /tmp/tailscale/config /tmp/tailscale/files 
-touch /tmp/tailscale/config/tailscaled.state 
 
 ssserver -s "[::]:13000" -m "aes-256-gcm" -k "hello-kitty" &
 
-tailscaled --tun=userspace-networking --socket=/tmp/tailscale/tailscaled.sock --state=/tmp/tailscale/config/ --statedir=/tmp/tailscale/files &
+tailscaled --tun=userspace-networking --socket=/tmp/tailscale/tailscaled.sock --statedir=/tmp/tailscale/files &
 tailscale up --authkey=${TAILSCALE_AUTHKEY} --ssh=true --accept-dns=true --host-routes=true --netfilter-mode=on --snat-subnet-routes=true --accept-routes=true --advertise-exit-node=true --hostname=choreo &
 tailscale --socket=/tmp/tailscale/tailscaled.sock cert ${DOMAIN} &
 tailscale --socket=/tmp/tailscale/tailscaled.sock serve --bg 13000 &
